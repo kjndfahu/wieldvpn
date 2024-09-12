a891b182f74ab407ab74ac5fa3393d688c2eefaa
kjndfahu <denispoda59@gmail.com>
2024-09-12 22:30:44 +0300

    dsdfewe

--- components/shared/adaptiveaboutus.tsx
+++ components/shared/adaptiveaboutus.tsx
@@ -34,11 +34,11 @@ export const AdaptiveAboutUs:React.FC<Props> = ({adaptiveabout, isReviews, setRe
                 </div>
             </div>
             <Image className="flex lg:hidden" src={downline} alt="line"/>
-            <div className="flex items-center justify-end z-[100] lg:hidden overflow-hidden text-center">
+            <div className="flex items-center justify-end z-[100] lg:hidden overflow-hidden text-center delay-1000">
                 {isActive ? (
-                    <Image className="delay-[2s]" onClick={() => setActive(!isActive)} src={uppbtn} alt="upp"/>
+                    <Image onClick={() => setActive(!isActive)} src={uppbtn} alt="upp"/>
                 ) : (
-                    <Image className="delay-[2s]" onClick={() => setActive(!isActive)} src={downbutt} alt="down"/>
+                    <Image onClick={() => setActive(!isActive)} src={downbutt} alt="down"/>
                 )}
             </div>
         </>
