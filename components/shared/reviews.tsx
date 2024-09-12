0f5591ce38c7afcd1ccdfdcd1d2e1964cb149651
kjndfahu <denispoda59@gmail.com>
2024-09-12 12:13:23 +0300

    qqqqwwwwwrrr

--- components/shared/reviews.tsx
+++ components/shared/reviews.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import lights from '../../assets/light behind review.png'
+import reviewlogo from '../../assets/animation (1).gif'
 import minilogo from '../../assets/prfile3d 1.png'
 import {SliderBlock} from "@/components/shared/sliderblock";
 import {useTranslations} from "next-intl";
@@ -17,6 +18,7 @@ export const Reviews: React.FC<Props> = ({reviews, className}) => {
             <Image className="absolute z-60 top-[250px] w-full" src={lights} alt="light"/>
             <h2 className="font-semibold text-white mdbvp:text-[96px] leading-[82px] mdbvp:ml-[136px] mdbvp:text-left sm:text-center  md:text-[64px] md:leading-[65px] smbvp:text-[40px] smbvp:leading-[45px]">{t('title')}<br/> {t('title2')}</h2>
             <Image className="self-center" src={minilogo} alt="minilogo"/>
+            <Image className="absolute z-1 right-0 lghero:top-[1400px] lghero:w-[500px] md:w-[400px] md:top-[1500px]" src={reviewlogo} alt="review"/>
             <SliderBlock/>
         </div>
     )
--- components/shared/sliderblock.tsx
+++ components/shared/sliderblock.tsx
@@ -20,28 +20,28 @@ export const SliderBlock: React.FC<Props> = ({className}) => {
     return (
         <div className="flex z-30 items-center justify-center mt-10 h-[50vh]">
             <Swiper
-                spaceBetween={70}
+                spaceBetween={50}
                 speed={1200}
                 centeredSlides={true}
-                autoplay={{
-                    delay: 1500,
-                    disableOnInteraction: false,
-                }}
+                // autoplay={{
+                //     delay: 1500,
+                //     disableOnInteraction: false,
+                // }}
                 allowTouchMove={false}
                 slidesPerView={2}
                 loop={true}
-                modules={[Autoplay]}
+                // modules={[Autoplay]}
                 className="flex items-center justify-center h-[40vh]">
 
-                <SwiperSlide className="text-white text-[32px] leading-[27px] w-[500px]">{t('text1')}</SwiperSlide>
+                <SwiperSlide className="text-white text-[32px] leading-[27px] w-[550px]">{t('text1')}</SwiperSlide>
 
-                <SwiperSlide className="text-white text-[32px] leading-[27px] w-[500px]">{t('text2')}</SwiperSlide>
+                <SwiperSlide className="text-white text-[32px] leading-[27px] w-[550px]">{t('text2')}</SwiperSlide>
 
-                <SwiperSlide className="text-white text-[32px] leading-[27px] w-[500px]">{t('text3')}</SwiperSlide>
+                <SwiperSlide className="text-white text-[32px] leading-[27px] w-[550px]">{t('text3')}</SwiperSlide>
 
-                <SwiperSlide className="text-white text-[32px] leading-[27px] w-[500px]">{t('text4')}</SwiperSlide>
+                <SwiperSlide className="text-white text-[32px] leading-[27px] w-[550px]">{t('text4')}</SwiperSlide>
 
-                <SwiperSlide className="text-white text-[32px] leading-[27px] w-[500px]">{t('text5')}</SwiperSlide>
+                <SwiperSlide className="text-white text-[32px] leading-[27px] w-[550px]">{t('text5')}</SwiperSlide>
 
             </Swiper>
         </div>
