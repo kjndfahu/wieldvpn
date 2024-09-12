283985412fac06aad1780a459301587f458723d5
kjndfahu <denispoda59@gmail.com>
2024-09-12 14:48:21 +0300

    dwewwww

--- components/shared/burgerpopup.tsx
+++ components/shared/burgerpopup.tsx
@@ -13,12 +13,12 @@ export const BurgerPopup: React.FC<Props> = ({className, isReviews, setReviews})
     return (
         <div className="flex flex-col absolute top-0 right-0 bg-[#333d41] w-[150px] h-[300px] [transition:1s] z-[100]">
             <div className="flex mt-[135px] flex-col gap-[2px] text-white font-bold backdrop-filter backdrop-blur-[20px]">
-                <div className="flex bg-white/20 w-full h-[40px] items-center justify-center">{t('reviews')}</div>
+                <div onClick={() => setReviews(true)} className="flex bg-white/20 w-full h-[40px] items-center justify-center">{t('reviews')}</div>
                 <div onClick={() => setReviews(false)}  className="flex bg-white/20 w-full h-[40px] items-center justify-center">{t('prices')}</div>
                 <Link href="https://help.wieldvpn.ru/">
                     <div className="flex bg-white/20 w-full h-[40px] items-center justify-center">{t('help')}</div>
                 </Link>
-                <div onClick={() => setReviews(!isReviews)} className="flex bg-white/20 w-full h-[40px] items-center justify-center">{t('aboutus')}</div>
+                <div onClick={() => setReviews(false)} className="flex bg-white/20 w-full h-[40px] items-center justify-center">{t('aboutus')}</div>
             </div>
         </div>
     )
