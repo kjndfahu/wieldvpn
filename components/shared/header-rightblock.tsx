95a17ee3840ccb815186c3b249ebaf0819031693
kjndfahu <denispoda59@gmail.com>
2024-09-12 21:10:57 +0300

    sdsadasd

--- components/shared/header-rightblock.tsx
+++ components/shared/header-rightblock.tsx
@@ -1,8 +1,9 @@
-
+'use client'
 
 import React from 'react'
 
 import {useTranslations} from "next-intl";
+import {motion} from 'framer-motion'
 import Link from "next/link";
 import {SortPopup} from "@/components/shared/sortpopup";
 
@@ -17,7 +18,7 @@ export const HeaderRightblock: React.FC<Props> = ({className}) => {
         <>
             <div className="lg:flex lg:gap-10 lg:text-white lg:text-[24px] lg:leading-6 lg:font-normal lg:mr-10 hidden items-center z-[100]">
                 <Link href="https://help.wieldvpn.ru/">
-                    <span className="cursor-pointerr">{t('help')}</span>
+                    <motion.div whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} className="cursor-pointerr">{t('help')}</motion.div>
                 </Link>
                 <SortPopup/>
             </div>
