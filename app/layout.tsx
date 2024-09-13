c74e0b38113c9eec52f2d3d97e05fd571593d106
kjndfahu <denispoda59@gmail.com>
2024-09-13 11:17:12 +0300

    [pdsdsd

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,19 @@
 import { ReactNode } from 'react';
+import {Montserrat} from "next/font/google";
 
 interface ErrorProps {
     children: ReactNode;
 }
 
+const montserrat = Montserrat({
+    weight: ['400', '600', '700'],
+    subsets: ['latin']
+})
+
 export default function GlobalError({ children }: ErrorProps) {
     return (
         <html style={{background: 'black'}}>
-        <body className="bg-black">
+        <body className={montserrat.className}>
         {children}
         </body>
         </html>
