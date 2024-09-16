d8d2b2a3ca87e2d68d881127edb496aabe4bf89e
kjndfahu <denispoda59@gmail.com>
2024-09-16 11:25:25 +0300

    Update buybtn.tsx

--- components/shared/buybtn.tsx
+++ components/shared/buybtn.tsx
@@ -17,7 +17,7 @@ export const Buybtn: React.FC<Props> = ({className}) => {
             <motion.div whileHover={{scale: 1.1}} whileTap={{scale: 0.9}}
                         className="rounded-[29px] flex cursor-pointer items-center justify-center mdbvp:w-[725px] mdbvp:h-[230px] [box-shadow:1px_25px_86px_28px_rgba(0,255,238,0.20)] md:w-[580px] md:h-[195px] smbvp:w-[300px] smbvp:h-[130px] w-[294px] h-[94px]"
                         style={{
-                            backgroundImage: "url('https://i.yapx.ru/X7wLG.png')"
+                            backgroundImage: "url('https://i.imgur.com/fUIABUs.png')"
                         }}>
                 <h2 className="md:text-[55px] text-white font-bold italic smbvp:text-[40px] text-[30px]">{t('btn')}</h2>
             </motion.div>
